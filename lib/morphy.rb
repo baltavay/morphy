5a962046d883c938569a9b849146f8c8d70ba174
idontcare <idontcare@idontcare-P5K-VM.(none)>
2015-05-30 00:22:22 +0600

    init

---
+++ lib/morphy.rb
@@ -0,0 +1,95 @@
+require "dawg"
+
+class Morphy
+  class Word
+    attr_accessor :para_id
+    
+    def initialize(word,para_id,index)
+      @word = word
+      @para_id = para_id.to_i
+      @index = index.to_i
+      @prefix_id = Morphy.paradigms[@para_id][@index*3]
+      @suffix_id = Morphy.paradigms[@para_id][@index*3+1]
+      @grammeme_id = Morphy.paradigms[@para_id][@index*3+2]
+      
+    end
+    
+    def to_s
+      @word      
+    end
+    
+    def normal_form
+      prefix = Morphy.prefixes[Morphy.paradigms[@para_id][0]]
+      suffix = Morphy.suffixes[Morphy.paradigms[@para_id][1]]      
+      
+      "#{prefix}#{stem}#{suffix}"
+    end
+    def grammemes
+      Morphy.grammemes[@grammeme_id]      
+    end
+    def stem
+      prefix = Morphy.prefixes[Morphy.paradigms[@para_id][0]]
+      suffix = Morphy.suffixes[Morphy.paradigms[@para_id][1]]
+      grammeme = Morphy.grammemes[Morphy.paradigms[@para_id][2]]
+      word = @word.dup
+      word.gsub!(Morphy.prefixes[@prefix_id],"")
+      word.gsub!(Morphy.suffixes[@suffix_id],"")
+      word
+    end
+
+    def same_paradigm?(other)
+      @para_id == other.para_id      
+    end
+
+    def tag
+      Morphy.grammemes[@grammeme_id].join(",") 
+    end
+
+    def lexemme
+      (0..(Morphy.paradigms[@para_id].length / 3)-1).map do |index|        
+        prefix = Morphy.prefixes[Morphy.paradigms[@para_id][index*3]]
+        suffix = Morphy.suffixes[Morphy.paradigms[@para_id][index*3+1]]
+        grammeme = Morphy.grammemes[Morphy.paradigms[@para_id][index*3+2]]
+        Word.new(prefix+stem+suffix,@para_id,index)
+      end      
+    end
+
+    def inflect(grammemes)
+      words = lexemme
+      words.each do |word|
+        return word if word.grammemes.last(grammemes.length) == grammemes
+      end
+      nil
+    end      
+  end
+  def initialize    
+    
+    path = File.dirname(__FILE__)+"/morphy/dictionary/"
+
+    @dawg = Dawg.load("#{path}/dawg.dat") # why it's eating so much memory?
+    @@suffixes ||= File.open("#{path}/suffixes.txt", 'r').read.split("\n")
+    @@prefixes ||= File.open("#{path}/prefixes.txt", 'r').read.split("\n")
+    @@grammemes ||= File.open("#{path}/grammemes.txt", 'r').read.split("\n").map{|g| g.split(",")}    
+    @@paradigms ||= Marshal.load(File.read("#{path}/paradigms.dat"))
+  end
+  def self.paradigms
+    @@paradigms    
+  end
+  def self.prefixes
+    @@prefixes    
+  end
+  def self.suffixes
+    @@suffixes
+  end
+  def self.grammemes
+    @@grammemes    
+  end
+  def find_similar(word)
+    results = @dawg.find_similar(word)
+    results = results.map do |result|
+      word,para_id,index = result.split(" ")
+      Word.new(word,para_id,index)
+    end
+    results
+  end
+end
---
+++ morphy.gemspec
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+Gem::Specification.new do |spec|
+  spec.name          = "morphy"
+  spec.version       = "0.0.1"
+  spec.date          = '2015-05-29'
+  spec.authors       = ["Maksatbek Mansurov"]
+  spec.email         = ["maksat.mansurov@gmail.com"]
+  spec.description   = %q{Morphological analyzer (POS tagger + inflection engine) for Russian language in ruby. Inspired by pymorphy2}
+  spec.summary       = %q{Morphological analyzer (POS tagger + inflection engine) for Russian language in ruby. Inspired by pymorphy2}
+  spec.homepage      = "https://github.com/baltavay/morphy"
+  spec.license       = "MIT"
+
+  spec.files         = `git ls-files`.split($/)
+  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
+  spec.require_paths = ["lib"]    
+  spec.add_development_dependency "dawg"
+end
